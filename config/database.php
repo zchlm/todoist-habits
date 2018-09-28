8c3cff91f67baab1229045b65ba82676f6d1307b
Zach Moore <zach@zachmoore.xyz>
2018-09-29 00:01:45 +1000

    Add support for Heroku pgsql #2

--- config/database.php
+++ config/database.php
@@ -1,6 +1,18 @@
 <?php
 
 $DATABASE_URL = parse_url(getenv('DATABASE_URL'));
+$pgsql = app()->environment('production') ? [
+    'driver' => 'pgsql',
+    'host' => $DATABASE_URL['host'],
+    'port' => $DATABASE_URL['port'],
+    'database' => ltrim($DATABASE_URL['path'], '/'),
+    'username' => $DATABASE_URL['user'],
+    'password' => $DATABASE_URL['pass'],
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
+    'sslmode' => 'require',
+] : [];
 
 return [
 
@@ -54,37 +66,26 @@ return [
         ],
 
         'sqlite' => [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => env('DB_DATABASE', base_path('database/database.sqlite')),
-            'prefix'   => env('DB_PREFIX', ''),
+            'prefix' => env('DB_PREFIX', ''),
         ],
 
         'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'port'      => env('DB_PORT', 3306),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
-            'charset'   => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix'    => env('DB_PREFIX', ''),
-            'timezone'  => env('DB_TIMEZONE', '+00:00'),
-            'strict'    => env('DB_STRICT_MODE', false),
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 5432),
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset'  => env('DB_CHARSET', 'utf8'),
-            'prefix'   => env('DB_PREFIX', ''),
-            'schema'   => env('DB_SCHEMA', 'public'),
-            'sslmode' => 'require',
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
+            'strict' => env('DB_STRICT_MODE', false),
         ],
+
+        'pgsql' => $pgsql
 
     ],
 
@@ -119,8 +120,8 @@ return [
         'cluster' => env('REDIS_CLUSTER', false),
 
         'default' => [
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
-            'port'     => env('REDIS_PORT', 6379),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DATABASE', 0),
             'password' => env('REDIS_PASSWORD', null),
         ],
